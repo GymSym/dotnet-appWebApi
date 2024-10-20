42c43e51c76b064dad1eec3727170d11f350c3b0
Mayank Srivastava <M.Srivastava2013@outlook.com>
2024-10-20 13:35:55 +0530

    First Model Contact

---
+++ Model/Contact.cs
@@ -0,0 +1,13 @@
+﻿namespace dotnet_appWebApi.Model
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+        public DateTime DateCreatedOn { get; set; } = DateTime.Now;
+
+        public bool IsDeleted { get; set; } = false;
+    }
+}
